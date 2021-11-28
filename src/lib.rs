26b3b21d31e5c1ab758cd5abbdf3e4d769d9aeca
Zixian Cai <u5937495@anu.edu.au>
2021-11-28 21:10:13 +1100

    MVP

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use base64::write::EncoderWriter;
+use hdrhistogram::serialization::Serializer;
+use hdrhistogram::serialization::V2DeflateSerializer;
+use hdrhistogram::Histogram;
+use std::fs::File;
+use std::path::Path;
+
+pub struct DaCapoLatencyDump {
+    hist: Histogram<u64>,
+}
+
+impl DaCapoLatencyDump {
+    pub fn new(path: &Path) -> Self {
+        let reader = File::open(path).unwrap();
+        Self::from_csv(reader)
+    }
+
+    fn from_csv<R: std::io::Read>(reader: R) -> Self {
+        let mut rdr = csv::Reader::from_reader(reader);
+        let mut hist = Histogram::<u64>::new(5).expect("Failed to create a histogram");
+        for result in rdr.records() {
+            let row = result.expect("Failed to read records");
+            let start: u64 = row[0].trim().parse().unwrap();
+            let end: u64 = row[1].trim().parse().unwrap();
+            let latency = end - start;
+            hist.record(latency).unwrap();
+        }
+        DaCapoLatencyDump { hist }
+    }
+
+    pub fn save_hdrh(&self, path: &Path) {
+        V2DeflateSerializer::new()
+            .serialize(
+                &self.hist,
+                &mut EncoderWriter::new(&mut File::create(path).unwrap(), base64::STANDARD),
+            )
+            .expect("Failed to save histogram as hdrh");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use dacapo_latency_dump_hdrh::DaCapoLatencyDump;
+use glob::glob;
+use indicatif::ParallelProgressIterator;
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use rayon::prelude::*;
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut entries: Vec<PathBuf> = (&args[1..])
+        .iter()
+        .map(|arg| {
+            let entries: Result<Vec<PathBuf>, _> =
+                glob(&format!("{}/**/dacapo-latency-usec-*.csv", arg))
+                    .expect("Failed to read glob pattern")
+                    .collect();
+            entries.expect("Failed to read entries")
+        })
+        .fold(Vec::new(), |mut acc, mut x| {
+            acc.append(&mut x);
+            acc
+        });
+    let pb = ProgressBar::new(entries.len() as u64);
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template(
+                "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})",
+            )
+            .progress_chars("#>-"),
+    );
+    entries.par_iter_mut().progress_with(pb).for_each(|entry| {
+        let dld = DaCapoLatencyDump::new(entry);
+        entry.set_extension("hdrh");
+        dld.save_hdrh(entry)
+    });
 }
